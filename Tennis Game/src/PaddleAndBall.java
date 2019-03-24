352c99340c505e20b1b8f52e4d1cbaf56a0e267b
HP <HP@DESKTOP-85IK0UN>
2019-03-24 16:51:51 +0530

    3rd Commit: Second paddle in the game

--- Tennis Game/src/PaddleAndBall.java
+++ Tennis Game/src/PaddleAndBall.java
@@ -7,11 +7,11 @@ public class PaddleAndBall implements ActionListener
 {
 	JFrame f;
 	Panel1 p1;
-	int x,y,a,b;
+	int x,y,a,b,preY;
 	public PaddleAndBall()
 	{
-		x=(int)(Math.random()*10);
-		y=(int)(Math.random()*10);
+		x=5;
+		y=3;
 		a=x;
 		b=y;
 		f=new JFrame();
@@ -28,26 +28,22 @@ public class PaddleAndBall implements ActionListener
 		Handler2 h2=new Handler2();
 		p1.addKeyListener(h2);
 		p1.requestFocusInWindow();//to shift focus from frame to panel
-		Timer t=new Timer(10,this);
+		Timer t=new Timer(12,this);
 		t.start();
 	}
 	private class Handler1 extends MouseMotionAdapter
 	{
 		public void mouseDragged(MouseEvent me)
 		{
-			if(me.getX()<0)
-				p1.mX=0;
-			else if(me.getY()<0)
+			if(me.getY()<0)
 				p1.mY=0;
-			else if(me.getX()>f.getWidth()-60)
-				p1.mX=f.getWidth()-60;
 			else if(me.getY()>f.getHeight()-60)
 				p1.mY=f.getHeight()-60;
 			else
 			{
-				p1.mX=me.getX();
 				p1.mY=me.getY();
 			}
+			preY=me.getY();
 			p1.repaint();
 		}
 	}
@@ -61,11 +57,11 @@ public class PaddleAndBall implements ActionListener
 			x=a;                       
 		if(p1.bY==0)
 			y=b;
-		if((p1.bY>=p1.mY && p1.bY<=p1.mY+60) && (p1.bX>=p1.mX && p1.bX<=p1.mX+20)) //(for reflection of ball after hitting paddle)
+		if((p1.bY>=p1.mY && p1.bY<=p1.mY+60) && (p1.bX==30 || p1.bX==550))//(for reflection of ball after hitting paddle)
 		{
 			x=-x;
 			y=-y;
-		} 
+		}
 		p1.bX+=x;
 		p1.bY+=y;
 		p1.repaint();
@@ -79,10 +75,6 @@ public class PaddleAndBall implements ActionListener
 				p1.mY-=20;
 			else if(key==KeyEvent.VK_DOWN)
 				p1.mY+=20;
-			else if(key==KeyEvent.VK_LEFT)
-				p1.mX-=10;
-			else if(key==KeyEvent.VK_RIGHT)
-				p1.mX+=10;
 			p1.repaint();
 		}
 		public void keyReleased(KeyEvent ke)
@@ -92,10 +84,6 @@ public class PaddleAndBall implements ActionListener
 				p1.mY-=20;
 			else if(key==KeyEvent.VK_DOWN)
 				p1.mY+=20;
-			else if(key==KeyEvent.VK_LEFT)
-				p1.mX-=10;
-			else if(key==KeyEvent.VK_RIGHT)
-				p1.mX+=10;
 			p1.repaint();
 		}
 		public void keyTyped(KeyEvent ke)
@@ -105,10 +93,6 @@ public class PaddleAndBall implements ActionListener
 				p1.mY-=20;
 			else if(key==KeyEvent.VK_DOWN)
 				p1.mY+=20;
-			else if(key==KeyEvent.VK_LEFT)
-				p1.mX-=10;
-			else if(key==KeyEvent.VK_RIGHT)
-				p1.mX+=10;
 			p1.repaint();
 		}
 	}
--- Tennis Game/src/Panel1.java
+++ Tennis Game/src/Panel1.java
@@ -6,13 +6,13 @@ public class Panel1 extends JPanel
 {
 	int bX=0;
 	int bY=0;
-	int mX=300;
 	int mY=300;
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
 		Graphics2D g1=(Graphics2D)g;
 		g1.fillOval(bX, bY, 20, 20);
-		g1.fillRect(mX, mY, 20, 60);
+		g1.fillRect(10, mY, 20, 60);
+		g1.fillRect(550, mY, 20, 60);
 	}
 }
